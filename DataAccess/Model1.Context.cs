588c7612c4203f2ce523efdfa58c75f2f66f80bd
MkFdez <mirkofs29@gmail.com>
2023-08-11 00:06:38 -0400

    now users can add their social media account and also in the publication/view you can go to the owner of the image profile by cliclking the profile picture

--- DataAccess/Model1.Context.cs
+++ DataAccess/Model1.Context.cs
@@ -40,6 +40,7 @@ namespace DataAccess
         public virtual DbSet<sysdiagram> sysdiagrams { get; set; }
         public virtual DbSet<DailySale> DailySales { get; set; }
         public virtual DbSet<MonthlySale> MonthlySales { get; set; }
+        public virtual DbSet<SocialMedia> SocialMedias { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
---
+++ Models/TemporalViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class TemporalViewModel
+    {
+        public ProfileViewModel Profile { get; set; }
+        public List<SimplePublicationViewModel> Publications { get; set; }
+    }
+}
